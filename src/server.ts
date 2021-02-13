84271f31ffa3a83bd0db17f7d31134ff3348d331
Pedro Brandão <phbrandao1206@gmial.com>
2021-02-13 22:49:23 +0100

    CORS added

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-import routes from './routes';
+import cors from 'cors';
 
+import routes from './routes';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 import './database/index';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
