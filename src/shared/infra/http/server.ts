0868720ee48d5642172922bdcd40e8d41b36c470
Pedro Brandão <phbrandao1206@gmial.com>
2021-03-19 04:44:09 +0100

    Add rate limiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,21 +1,23 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import rateLimiter from './middlewares/rateLimiter';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
-
-import uploadConfig from '@config/upload';
-import AppError from '@shared/errors/AppError';
 
 import routes from './routes';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
