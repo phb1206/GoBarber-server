d71306e17dc2a946c25b6c96f74a43a80c4defc7
Pedro Brandão <phbrandao1206@gmial.com>
2021-03-20 03:22:54 +0100

    fix: date not available after day has passed

--- src/modules/appointment/services/ListMonthAvailabilityService.ts
+++ src/modules/appointment/services/ListMonthAvailabilityService.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
-import { getDaysInMonth } from 'date-fns';
+import { getDaysInMonth, isAfter } from 'date-fns';
 import AppointmentRepository from '../repositories/IAppointmentRepository';
 
 interface IRequest {
@@ -38,11 +38,18 @@ class ListMonthAvailabilityService {
         );
 
         const availability = monthArray.map(day => {
+            const compareDate = new Date(year, month - 1, day, 17, 59, 59);
+
             const appointmentsInDay = appointments.filter(
                 appointment => appointment.date.getDate() === day,
             );
 
-            return { day, isAvailable: appointmentsInDay.length < 10 };
+            return {
+                day,
+                isAvailable:
+                    isAfter(compareDate, new Date()) &&
+                    appointmentsInDay.length < 10,
+            };
         });
         return availability;
     }
